7996403e9eda5122cd6f26f35b7a20d9497be661
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:55:02 +0200

    created file exercises/9553263a-b6db-4770-bd26-e47c4aa328c1/skeletons/920de1a8-28a0-4b99-b6e5-59299077c2c1/in.py

---
+++ exercises/9553263a-b6db-4770-bd26-e47c4aa328c1/skeletons/920de1a8-28a0-4b99-b6e5-59299077c2c1/in.py
@@ -0,0 +1,10 @@
+inventory = {}
+def add_droid(name, type):
+    # Add your code here
+
+def display_inventory():
+    # Add your code here
+
+# Example usage
+add_droid('R2-D2', 'Astromech')
+display_inventory()
