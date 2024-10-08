400b22b03249942a7579e6aff4f3f40bfd0bf0f6
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:48:47 +0200

    created file exercises/54d7e76b-1f20-4c02-b7b5-879a996a571f/solutions/25467b41-b2bc-4d5b-a583-c731abb5311f/sol.py

---
+++ exercises/54d7e76b-1f20-4c02-b7b5-879a996a571f/solutions/25467b41-b2bc-4d5b-a583-c731abb5311f/sol.py
@@ -0,0 +1,9 @@
+droids = ['R2-D2', 'C-3PO', 'BB-8']
+for droid in droids:
+    print('Droid: ' + droid)
+    if droid == 'R2-D2':
+        print('Astromech Droid!')
+    elif droid == 'C-3PO':
+        print('Protocol Droid!')
+    else:
+        print('Unknown Droid!')
