f06954c710cbac047b64d526b74a8030cea03d63
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 21:54:08 +0200

    created file exercises/1009dbad-6e1d-4c47-958d-4102a65ca8ab/skeletons/84bae128-a940-4c94-9ff6-5a7e918f47f6/in.py

---
+++ exercises/1009dbad-6e1d-4c47-958d-4102a65ca8ab/skeletons/84bae128-a940-4c94-9ff6-5a7e918f47f6/in.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+    
+    def add_points(self, points):
+        # Add points to the house
+        pass
+    
+    def subtract_points(self, points):
+        # Subtract points from the house
+        pass
