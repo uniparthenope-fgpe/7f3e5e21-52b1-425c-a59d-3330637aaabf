e1ced32aeec5e62f6748357ccae7789386f7caef
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 21:47:33 +0200

    created file exercises/9f09016a-72cd-4648-8fcb-cdfc9b79d96b/skeletons/4d43be22-8ea0-4af4-af86-6e7c7a182a20/in.py

---
+++ exercises/9f09016a-72cd-4648-8fcb-cdfc9b79d96b/skeletons/4d43be22-8ea0-4af4-af86-6e7c7a182a20/in.py
@@ -0,0 +1,12 @@
+class MagicalCreature:
+    def __init__(self, name):
+        self.name = name
+        self.registry = []
+    
+    def add_creature(self, creature):
+        # Add creature to the registry
+        pass
+    
+    def find_creature(self, name):
+        # Search for a creature by name
+        pass
