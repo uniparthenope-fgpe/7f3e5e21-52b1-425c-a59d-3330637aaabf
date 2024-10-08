040364c0f4a6ae7b39e0e4ddb4c2fb6e59d22e78
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:37:24 +0200

    created file exercises/c1dc7687-9119-4eba-ad58-81f69e9a951e/skeletons/9c4e8d66-98b4-4fd2-9f3b-22ec98e34054/in.py

---
+++ exercises/c1dc7687-9119-4eba-ad58-81f69e9a951e/skeletons/9c4e8d66-98b4-4fd2-9f3b-22ec98e34054/in.py
@@ -0,0 +1,7 @@
+def optimize_route(planets):
+    # Implement the logic to find the shortest route
+    pass
+
+planets = ['Tatooine', 'Hoth', 'Endor']
+shortest_route = optimize_route(planets)
+print('Shortest route:', shortest_route)
