6c6fe03170a43ec8267331f65525bd372ef111ec
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:29:43 +0200

    created file exercises/19a526d6-5c13-4e5b-af57-6f1044950963/skeletons/61a904df-60dd-4d12-93ab-0fbaaf36d1d9/in.py

---
+++ exercises/19a526d6-5c13-4e5b-af57-6f1044950963/skeletons/61a904df-60dd-4d12-93ab-0fbaaf36d1d9/in.py
@@ -0,0 +1,6 @@
+def recursive_sort(sith_names):
+    # Implement the sorting logic here
+    pass
+
+sith_names = ['Darth Vader', 'Darth Maul', 'Darth Sidious']
+sorted_names = recursive_sort(sith_names)
