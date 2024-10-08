c69f7f631ff878d9ef67fac6fb2e3e63cb884263
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:36:24 +0200

    created file exercises/8ca875af-9527-4a33-a80f-15a559c2c235/solutions/b6b7b3e7-7b52-4f20-985a-f344f34acb0d/sol.py

---
+++ exercises/8ca875af-9527-4a33-a80f-15a559c2c235/solutions/b6b7b3e7-7b52-4f20-985a-f344f34acb0d/sol.py
@@ -0,0 +1,9 @@
+def sorting_hat(trait):
+    if trait == 'bravery':
+        return 'Gryffindor'
+    elif trait == 'intelligence':
+        return 'Ravenclaw'
+    elif trait == 'loyalty':
+        return 'Hufflepuff'
+    else:
+        return 'Slytherin'
