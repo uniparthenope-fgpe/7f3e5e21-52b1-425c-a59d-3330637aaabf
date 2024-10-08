e28935d7af15ee949439454f403353d4566a177d
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:56:47 +0200

    created file exercises/13908128-f98d-41e6-a44e-d39956ea0aa0/solutions/134956aa-31f8-4f62-93e1-72d34d53d0f2/sol.py

---
+++ exercises/13908128-f98d-41e6-a44e-d39956ea0aa0/solutions/134956aa-31f8-4f62-93e1-72d34d53d0f2/sol.py
@@ -0,0 +1,6 @@
+def sort_inventory(items):
+    return sorted(items)
+
+inventory = ['Blaster', 'Astromech Droid', 'X-Wing', 'TIE Fighter']
+sorted_inventory = sort_inventory(inventory)
+print(sorted_inventory)
