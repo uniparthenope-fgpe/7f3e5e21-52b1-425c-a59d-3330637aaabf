10f6dc7fa724e2f398a1e1fafe97c018b28b3a51
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:50:41 +0200

    created file exercises/d074d63c-229b-43fc-9f06-563c2dda72c8/skeletons/39c51a0d-4584-4a4a-91e5-9548cfdd684b/in.py

---
+++ exercises/d074d63c-229b-43fc-9f06-563c2dda72c8/skeletons/39c51a0d-4584-4a4a-91e5-9548cfdd684b/in.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Scheduler:
+    def __init__(self):
+        self.schedule = []
+
+    def add_event(self, event, time):
+        # Your code here
+
+    def view_schedule(self):
+        return self.schedule
