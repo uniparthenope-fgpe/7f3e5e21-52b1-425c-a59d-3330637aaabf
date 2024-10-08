0f8ab152fb3d77025f0d8d80aac68a3dba9d13e5
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:15:42 +0200

    created file exercises/4153215b-d87b-401b-9ad1-f3ed8a1e78a6/skeletons/c84245d8-0c79-42b6-a16c-61d4ac84959c/in.py

---
+++ exercises/4153215b-d87b-401b-9ad1-f3ed8a1e78a6/skeletons/c84245d8-0c79-42b6-a16c-61d4ac84959c/in.py
@@ -0,0 +1,10 @@
+class MagicalCreature:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    
+class Encyclopedia:
+    def __init__(self):
+        self.creatures = []
+    
+    # Add methods for add, remove, and search
