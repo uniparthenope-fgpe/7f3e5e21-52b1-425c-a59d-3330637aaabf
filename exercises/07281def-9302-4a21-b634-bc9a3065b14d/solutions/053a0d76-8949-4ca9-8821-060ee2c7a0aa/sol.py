60978b60030f7a9fecd4afd97b6ac09a89fc6c8b
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:21:37 +0200

    created file exercises/07281def-9302-4a21-b634-bc9a3065b14d/solutions/053a0d76-8949-4ca9-8821-060ee2c7a0aa/sol.py

---
+++ exercises/07281def-9302-4a21-b634-bc9a3065b14d/solutions/053a0d76-8949-4ca9-8821-060ee2c7a0aa/sol.py
@@ -0,0 +1,11 @@
+def sort_students(name):
+    last_name = name.split()[-1]
+    first_letter = last_name[0]
+    if first_letter in ['A', 'B', 'C']:
+        return 'Gryffindor'
+    elif first_letter in ['D', 'E', 'F']:
+        return 'Hufflepuff'
+    elif first_letter in ['G', 'H', 'I']:
+        return 'Ravenclaw'
+    else:
+        return 'Slytherin'
