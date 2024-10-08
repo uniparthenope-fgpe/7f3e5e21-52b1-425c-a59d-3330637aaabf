565d5bdca530392a1f5510ba12ce30af14d145fe
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:45:21 +0200

    created file exercises/f128fcd8-e4cd-4a6f-ade4-54a3438b6e44/solutions/8a5c56c6-50c7-4dae-ad3c-f41efdb69bb4/sol.py

---
+++ exercises/f128fcd8-e4cd-4a6f-ade4-54a3438b6e44/solutions/8a5c56c6-50c7-4dae-ad3c-f41efdb69bb4/sol.py
@@ -0,0 +1,5 @@
+def calculate_galactic_age(earth_years):
+    galactic_years = earth_years / 1.5
+    return galactic_years
+# Test the function
+print(calculate_galactic_age(30))  # Expected output: 20.0
