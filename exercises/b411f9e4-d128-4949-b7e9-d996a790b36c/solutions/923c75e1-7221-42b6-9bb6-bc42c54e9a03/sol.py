6b1fe58f8b1d2718075c5637fecf699c520c90b4
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:28:27 +0200

    created file exercises/b411f9e4-d128-4949-b7e9-d996a790b36c/solutions/923c75e1-7221-42b6-9bb6-bc42c54e9a03/sol.py

---
+++ exercises/b411f9e4-d128-4949-b7e9-d996a790b36c/solutions/923c75e1-7221-42b6-9bb6-bc42c54e9a03/sol.py
@@ -0,0 +1,7 @@
+ingredients = ['eye of newt', 'dragon blood', 'basilisk fang']
+required = ['eye of newt', 'dragon blood', 'unicorn hair']
+for item in required:
+    if item not in ingredients:
+        print('Missing:', item)
+    else:
+        print('All ingredients present')
