092745a8f9a49af6a51d3eeafb84ed30b7703199
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 00:07:17 +0200

    created file exercises/db778054-9240-4785-ac3d-92581a3dfe43/solutions/a28c552a-56d0-4111-9a8e-2f26ff266d42/sol.py

---
+++ exercises/db778054-9240-4785-ac3d-92581a3dfe43/solutions/a28c552a-56d0-4111-9a8e-2f26ff266d42/sol.py
@@ -0,0 +1,9 @@
+def lightsaber_color(rank):
+    if rank == 'Padawan':
+        return 'blue'
+    elif rank == 'Knight':
+        return 'green'
+    elif rank == 'Master':
+        return 'purple'
+    else:
+        return 'unknown'
