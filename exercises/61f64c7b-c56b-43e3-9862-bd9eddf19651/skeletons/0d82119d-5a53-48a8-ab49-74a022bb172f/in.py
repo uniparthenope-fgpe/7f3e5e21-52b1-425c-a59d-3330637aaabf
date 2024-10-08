f9d76fc4a324516aef726240d5d49dc41953e036
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 00:08:52 +0200

    created file exercises/61f64c7b-c56b-43e3-9862-bd9eddf19651/skeletons/0d82119d-5a53-48a8-ab49-74a022bb172f/in.py

---
+++ exercises/61f64c7b-c56b-43e3-9862-bd9eddf19651/skeletons/0d82119d-5a53-48a8-ab49-74a022bb172f/in.py
@@ -0,0 +1,7 @@
+def travel_itinerary(planets, start):
+    itinerary = [start]
+    # Add logic to visit each planet
+    return itinerary
+
+# Example usage:
+print(travel_itinerary(['Tatooine', 'Hoth', 'Endor'], 'Tatooine'))
