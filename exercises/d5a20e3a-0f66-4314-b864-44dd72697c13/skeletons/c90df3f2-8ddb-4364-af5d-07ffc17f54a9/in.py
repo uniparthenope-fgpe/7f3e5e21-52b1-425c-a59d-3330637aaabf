27f88e49e034554886d31d594b5e2ecfbe51b350
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 00:16:16 +0200

    created file exercises/d5a20e3a-0f66-4314-b864-44dd72697c13/skeletons/c90df3f2-8ddb-4364-af5d-07ffc17f54a9/in.py

---
+++ exercises/d5a20e3a-0f66-4314-b864-44dd72697c13/skeletons/c90df3f2-8ddb-4364-af5d-07ffc17f54a9/in.py
@@ -0,0 +1,4 @@
+droids = [{'name': 'R2-D2', 'type': 'Astromech'}, {'name': 'C-3PO', 'type': 'Protocol'}]
+for droid in droids:
+    print('Droid Name: ' + droid['name'] + ' - Type: ' + droid['type'])
+# Fix the bug in the code above.
