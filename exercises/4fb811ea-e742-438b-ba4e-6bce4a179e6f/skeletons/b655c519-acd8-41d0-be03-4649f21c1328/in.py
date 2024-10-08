b129a61ebcb7b19d27a6741a7dc21eacbdd6886b
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 21:50:21 +0200

    created file exercises/4fb811ea-e742-438b-ba4e-6bce4a179e6f/skeletons/b655c519-acd8-41d0-be03-4649f21c1328/in.py

---
+++ exercises/4fb811ea-e742-438b-ba4e-6bce4a179e6f/skeletons/b655c519-acd8-41d0-be03-4649f21c1328/in.py
@@ -0,0 +1,16 @@
+class Event:
+    def __init__(self, title, time):
+        self.title = title
+        self.time = time
+
+class Scheduler:
+    def __init__(self):
+        self.events = []
+    
+    def add_event(self, event):
+        # Add event to the schedule
+        pass
+    
+    def show_schedule(self):
+        # Display all events
+        pass
