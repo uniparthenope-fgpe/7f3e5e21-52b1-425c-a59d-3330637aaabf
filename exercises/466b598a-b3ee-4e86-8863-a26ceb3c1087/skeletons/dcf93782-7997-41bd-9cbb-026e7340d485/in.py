8ad7ca14d356b3a274dab419b30957d025e461a8
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:57:22 +0200

    created file exercises/466b598a-b3ee-4e86-8863-a26ceb3c1087/skeletons/dcf93782-7997-41bd-9cbb-026e7340d485/in.py

---
+++ exercises/466b598a-b3ee-4e86-8863-a26ceb3c1087/skeletons/dcf93782-7997-41bd-9cbb-026e7340d485/in.py
@@ -0,0 +1,5 @@
+droids = {'R2-D2': 'Astromech', 'C-3PO': 'Protocol'}
+# Extend the program to list all droids
+# Fill in the gaps below
+for name, function in droids.items():
+    print(f'{name} is a {function} droid.')
