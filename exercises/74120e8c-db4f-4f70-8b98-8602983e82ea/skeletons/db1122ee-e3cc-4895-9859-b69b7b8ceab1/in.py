db4d246d5076a448a71b486fc1673dae485f0dde
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:24:08 +0200

    created file exercises/74120e8c-db4f-4f70-8b98-8602983e82ea/skeletons/db1122ee-e3cc-4895-9859-b69b7b8ceab1/in.py

---
+++ exercises/74120e8c-db4f-4f70-8b98-8602983e82ea/skeletons/db1122ee-e3cc-4895-9859-b69b7b8ceab1/in.py
@@ -0,0 +1,7 @@
+import random
+
+ingredients = ['Dragon Heartstring', 'Phoenix Feather', 'Unicorn Hair']
+
+potion_name = input('Enter the potion name: ')
+selected_ingredient = ________
+print(f'The ingredient for {potion_name} is {selected_ingredient}.')
