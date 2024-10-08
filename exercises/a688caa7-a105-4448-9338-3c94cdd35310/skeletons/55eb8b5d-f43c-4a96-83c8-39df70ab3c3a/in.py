f5e30530dab4cbd79bcd97ba1ee6b320e2c0332c
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:54:05 +0200

    created file exercises/a688caa7-a105-4448-9338-3c94cdd35310/skeletons/55eb8b5d-f43c-4a96-83c8-39df70ab3c3a/in.py

---
+++ exercises/a688caa7-a105-4448-9338-3c94cdd35310/skeletons/55eb8b5d-f43c-4a96-83c8-39df70ab3c3a/in.py
@@ -0,0 +1,8 @@
+class Starship:
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed
+
+    # Add a method to calculate total speed
+
+# Create instances of Starship and calculate total speed
