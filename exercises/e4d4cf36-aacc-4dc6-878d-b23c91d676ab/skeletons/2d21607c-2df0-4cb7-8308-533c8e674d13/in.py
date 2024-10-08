089d7a971439c46022a360605c7143507af896ea
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:20:03 +0200

    created file exercises/e4d4cf36-aacc-4dc6-878d-b23c91d676ab/skeletons/2d21607c-2df0-4cb7-8308-533c8e674d13/in.py

---
+++ exercises/e4d4cf36-aacc-4dc6-878d-b23c91d676ab/skeletons/2d21607c-2df0-4cb7-8308-533c8e674d13/in.py
@@ -0,0 +1,7 @@
+class MagicalCreature:
+    def __init__(self, name, creature_type):
+        self.name = name
+        self.creature_type = creature_type
+    # Add a method that describes the creature
+    def describe(self):
+        pass
