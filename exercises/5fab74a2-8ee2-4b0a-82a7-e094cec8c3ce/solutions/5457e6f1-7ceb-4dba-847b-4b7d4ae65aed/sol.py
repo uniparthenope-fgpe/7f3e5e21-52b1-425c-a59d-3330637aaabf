cd50b4ac6f7d45d17458643bffe67e85a59b06d9
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 21:59:59 +0200

    created file exercises/5fab74a2-8ee2-4b0a-82a7-e094cec8c3ce/solutions/5457e6f1-7ceb-4dba-847b-4b7d4ae65aed/sol.py

---
+++ exercises/5fab74a2-8ee2-4b0a-82a7-e094cec8c3ce/solutions/5457e6f1-7ceb-4dba-847b-4b7d4ae65aed/sol.py
@@ -0,0 +1,9 @@
+def sorting_hat(preference):
+    if preference == 'bravery':
+        return 'Gryffindor'
+    elif preference == 'intelligence':
+        return 'Ravenclaw'
+    elif preference == 'loyalty':
+        return 'Hufflepuff'
+    else:
+        return 'Slytherin'
