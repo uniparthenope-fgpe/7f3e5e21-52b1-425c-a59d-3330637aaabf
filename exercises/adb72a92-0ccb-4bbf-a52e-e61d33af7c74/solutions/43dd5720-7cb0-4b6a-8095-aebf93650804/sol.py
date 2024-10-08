44650879f22c681e660a5d86b6c3dc12274a7a0c
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:08:58 +0200

    created file exercises/adb72a92-0ccb-4bbf-a52e-e61d33af7c74/solutions/43dd5720-7cb0-4b6a-8095-aebf93650804/sol.py

---
+++ exercises/adb72a92-0ccb-4bbf-a52e-e61d33af7c74/solutions/43dd5720-7cb0-4b6a-8095-aebf93650804/sol.py
@@ -0,0 +1,7 @@
+color = input('Enter your favorite color: ')
+if color == 'blue':
+    print('Your lightsaber is blue!')
+elif color == 'red':
+    print('Your lightsaber is red!')
+else:
+    print('Your lightsaber is green!')
