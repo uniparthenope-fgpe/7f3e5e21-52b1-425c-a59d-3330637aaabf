1b995fdc7ecc6ed0c217414dcd2df5593e06b01d
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:04:10 +0200

    created file exercises/bb087e0a-e848-4a81-9ec1-d02cd4d0144b/skeletons/bd38bb7e-d3d8-48c3-bd6e-42600437127a/in.py

---
+++ exercises/bb087e0a-e848-4a81-9ec1-d02cd4d0144b/skeletons/bd38bb7e-d3d8-48c3-bd6e-42600437127a/in.py
@@ -0,0 +1,9 @@
+droid_inventory = {}
+def add_droid(name, model):
+    # Add droid to inventory
+
+def remove_droid(name):
+    # Remove droid from inventory
+
+def list_droids():
+    # List all droids in inventory
