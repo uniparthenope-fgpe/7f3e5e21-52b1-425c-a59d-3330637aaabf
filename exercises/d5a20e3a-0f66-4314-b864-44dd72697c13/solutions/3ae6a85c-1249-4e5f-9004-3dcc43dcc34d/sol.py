5b88178ede7281f58ac1f5cbbae5d12bc0839a1a
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 00:09:56 +0200

    created file exercises/d5a20e3a-0f66-4314-b864-44dd72697c13/solutions/3ae6a85c-1249-4e5f-9004-3dcc43dcc34d/sol.py

---
+++ exercises/d5a20e3a-0f66-4314-b864-44dd72697c13/solutions/3ae6a85c-1249-4e5f-9004-3dcc43dcc34d/sol.py
@@ -0,0 +1,3 @@
+droids = [{'name': 'R2-D2', 'type': 'Astromech'}, {'name': 'C-3PO', 'type': 'Protocol'}]
+for droid in droids:
+    print('Droid Name: ' + droid['name'] + ' - Type: ' + droid['type'])
