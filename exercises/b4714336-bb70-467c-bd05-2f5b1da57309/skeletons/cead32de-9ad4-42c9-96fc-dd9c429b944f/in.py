0ab5643b109b7ce1055a3b930787ddc41e8f7de8
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:53:41 +0200

    created file exercises/b4714336-bb70-467c-bd05-2f5b1da57309/skeletons/cead32de-9ad4-42c9-96fc-dd9c429b944f/in.py

---
+++ exercises/b4714336-bb70-467c-bd05-2f5b1da57309/skeletons/cead32de-9ad4-42c9-96fc-dd9c429b944f/in.py
@@ -0,0 +1,10 @@
+def add_droid(inventory, droid):
+    inventory.append(droid)
+    return inventory
+
+inventory = ['R2-D2', 'C-3PO']
+new_droid = 'BB-8'
+
+# Call the function to add the new droid
+add_droid(inventory, new_droid)
+print(inventory)
