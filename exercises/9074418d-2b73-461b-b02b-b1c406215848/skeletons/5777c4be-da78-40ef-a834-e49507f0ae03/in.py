40dbf3f69571073919dfcd0351c4ff8208f67159
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:25:35 +0200

    created file exercises/9074418d-2b73-461b-b02b-b1c406215848/skeletons/5777c4be-da78-40ef-a834-e49507f0ae03/in.py

---
+++ exercises/9074418d-2b73-461b-b02b-b1c406215848/skeletons/5777c4be-da78-40ef-a834-e49507f0ae03/in.py
@@ -0,0 +1,7 @@
+def sorting_hat(student_name):
+    if student_name in ['Harry', 'Hermione', 'Ron']:
+        return 'Gryffindor'
+    elif student_name in ['Draco', 'Pansy']:
+        return 'Slytherin'
+    else:
+        return 'Unknown'
