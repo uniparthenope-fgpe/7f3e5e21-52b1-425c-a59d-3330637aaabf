1f894ab5a5c256da23f032611dc5a96cd7684141
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:44:06 +0200

    created file exercises/2e2bc6d2-209c-4c7d-974a-786bf3031c3c/skeletons/9a27ae69-eeb5-4bef-8ded-610a09d851fc/in.py

---
+++ exercises/2e2bc6d2-209c-4c7d-974a-786bf3031c3c/skeletons/9a27ae69-eeb5-4bef-8ded-610a09d851fc/in.py
@@ -0,0 +1,9 @@
+import math
+
+def convert_coordinates(x, y):
+    # Complete the function
+    distance = 
+    return distance
+
+# Example usage
+print(convert_coordinates(3, 4))  # Should return 5.0
