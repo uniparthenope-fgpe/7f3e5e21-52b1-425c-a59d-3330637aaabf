d12cf7d4e897cf2750e40a412ee9853c3c83378d
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:15:09 +0200

    created file exercises/f3303467-f848-41b5-897e-702903155e7d/skeletons/4e3237a0-d173-4183-b8a2-12bd68ebf948/in.py

---
+++ exercises/f3303467-f848-41b5-897e-702903155e7d/skeletons/4e3237a0-d173-4183-b8a2-12bd68ebf948/in.py
@@ -0,0 +1,13 @@
+class Potion:
+    def __init__(self, name):
+        self.name = name
+        self.ingredients = []
+
+    def add_ingredient(self, ingredient):
+        # Implement this method
+
+    def remove_ingredient(self, ingredient):
+        # Implement this method
+
+    def get_ingredients(self):
+        return self.ingredients
