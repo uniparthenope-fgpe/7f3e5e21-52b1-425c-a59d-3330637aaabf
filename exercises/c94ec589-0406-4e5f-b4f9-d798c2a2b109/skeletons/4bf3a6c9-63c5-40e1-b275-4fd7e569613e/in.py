96532fe0728ad6786591404c64ba64c84a370861
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:20:06 +0200

    created file exercises/c94ec589-0406-4e5f-b4f9-d798c2a2b109/skeletons/4bf3a6c9-63c5-40e1-b275-4fd7e569613e/in.py

---
+++ exercises/c94ec589-0406-4e5f-b4f9-d798c2a2b109/skeletons/4bf3a6c9-63c5-40e1-b275-4fd7e569613e/in.py
@@ -0,0 +1,6 @@
+def brew_potion(ingredients):
+    potion_name = ''
+    # Complete the function below
+    if 'unicorn hair' in ingredients and 'dragon blood' in ingredients:
+        potion_name = 'Elixir of Life'
+    return potion_name
