8f34541a47d1e1a33455073ab122c3001cb24638
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 00:10:39 +0200

    created file exercises/0ac8769f-b99a-4334-927f-93b8c0974d87/skeletons/b0dd4c44-b061-4720-a598-4ac9263cc024/in.py

---
+++ exercises/0ac8769f-b99a-4334-927f-93b8c0974d87/skeletons/b0dd4c44-b061-4720-a598-4ac9263cc024/in.py
@@ -0,0 +1,8 @@
+class Spaceship:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    # Add attack method here
+    # Add defense method here
+
+# Create a spaceship object and test the methods.
