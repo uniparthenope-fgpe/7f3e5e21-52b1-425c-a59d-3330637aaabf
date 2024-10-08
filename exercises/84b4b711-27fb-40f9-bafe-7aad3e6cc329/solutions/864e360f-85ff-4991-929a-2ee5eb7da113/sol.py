3bc0c38026e1e04f661a5485fbfdb5eee2b81c19
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:34:14 +0200

    created file exercises/84b4b711-27fb-40f9-bafe-7aad3e6cc329/solutions/864e360f-85ff-4991-929a-2ee5eb7da113/sol.py

---
+++ exercises/84b4b711-27fb-40f9-bafe-7aad3e6cc329/solutions/864e360f-85ff-4991-929a-2ee5eb7da113/sol.py
@@ -0,0 +1,9 @@
+color = input('What is your favorite color? ')
+if color == 'blue':
+    print('You wield a blue lightsaber!')
+elif color == 'green':
+    print('You wield a green lightsaber!')
+elif color == 'red':
+    print('You wield a red lightsaber!')
+else:
+    print('You wield a lightsaber of a different color!')
