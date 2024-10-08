2f03981451023e42985b42d4b479711b47a0652b
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:55:36 +0200

    created file exercises/d66718b0-240e-441e-9512-50c53a180b3a/skeletons/abdc9b01-cc41-4687-be68-cfbda5df0a9c/in.py

---
+++ exercises/d66718b0-240e-441e-9512-50c53a180b3a/skeletons/abdc9b01-cc41-4687-be68-cfbda5df0a9c/in.py
@@ -0,0 +1,8 @@
+favorite_color = input('Enter your favorite color: ')
+# Fill in the gaps below
+if favorite_color == 'blue':
+    print('You wield a blue lightsaber!')
+elif favorite_color == 'green':
+    print('You wield a green lightsaber!')
+else:
+    print('You wield a lightsaber of a different color!')
