c32f9d0fc528e217eff9210c417c8dea8efd4a74
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:34:35 +0200

    created file exercises/c1dc7687-9119-4eba-ad58-81f69e9a951e/solutions/e12473a2-0774-4a98-bf2d-7b0740bc6c80/sol.py

---
+++ exercises/c1dc7687-9119-4eba-ad58-81f69e9a951e/solutions/e12473a2-0774-4a98-bf2d-7b0740bc6c80/sol.py
@@ -0,0 +1,7 @@
+def optimize_route(planets):
+    # Example implementation using a simple heuristic
+    return sorted(planets)
+
+planets = ['Tatooine', 'Hoth', 'Endor']
+shortest_route = optimize_route(planets)
+print('Shortest route:', shortest_route)
