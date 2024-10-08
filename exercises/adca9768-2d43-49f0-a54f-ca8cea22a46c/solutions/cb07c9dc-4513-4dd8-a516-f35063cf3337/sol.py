35c5510cb40d3db43a5c392feb74a682d3883a7f
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:08:46 +0200

    created file exercises/adca9768-2d43-49f0-a54f-ca8cea22a46c/solutions/cb07c9dc-4513-4dd8-a516-f35063cf3337/sol.py

---
+++ exercises/adca9768-2d43-49f0-a54f-ca8cea22a46c/solutions/cb07c9dc-4513-4dd8-a516-f35063cf3337/sol.py
@@ -0,0 +1,6 @@
+creatures = {'Unicorn': 'A magical horse', 'Dragon': 'A fire-breathing creature'}
+
+def search_creature(name):
+    return creatures.get(name, 'Creature not found')
+
+# The function is correct; no bugs to fix.
