5af349f83007cd6525b8f5b697a51c94b5b9a3cb
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:34:17 +0200

    created file exercises/03371525-aa2f-4589-980b-89864a88a784/skeletons/d337c82a-01e5-490d-a3a6-4e1e20a61a06/in.py

---
+++ exercises/03371525-aa2f-4589-980b-89864a88a784/skeletons/d337c82a-01e5-490d-a3a6-4e1e20a61a06/in.py
@@ -0,0 +1,7 @@
+droids = ['R2-D2', 'BB-8', 'C-3PO']
+count = 0
+for droid in droids:
+    count += 1
+print('Total droids:', count)
+
+# Bug: The count should be increased by 1 only if the droid is not None.
