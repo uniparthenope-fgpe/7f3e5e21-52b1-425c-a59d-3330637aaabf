c05b5f9110014cb35e20fe4352acb6a3ac408979
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:32:04 +0200

    created file exercises/62ff78ae-f77c-4547-98d2-8c377842f0c2/skeletons/4082ac8c-a99e-4690-a4a4-c0a0cb5ff9e0/in.py

---
+++ exercises/62ff78ae-f77c-4547-98d2-8c377842f0c2/skeletons/4082ac8c-a99e-4690-a4a4-c0a0cb5ff9e0/in.py
@@ -0,0 +1,5 @@
+def sorting_hat(students):
+    houses = {'Gryffindor': [], 'Hufflepuff': [], 'Ravenclaw': [], 'Slytherin': []}
+    for student in students:
+        # Fill in the gaps to sort students into houses
+    return houses
