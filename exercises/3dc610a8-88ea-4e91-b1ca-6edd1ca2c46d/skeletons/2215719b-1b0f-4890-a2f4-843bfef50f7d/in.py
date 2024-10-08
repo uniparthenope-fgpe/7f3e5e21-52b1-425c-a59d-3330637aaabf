515a551f8e0ae6065d16540fe571434f36f42971
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:09:31 +0200

    created file exercises/3dc610a8-88ea-4e91-b1ca-6edd1ca2c46d/skeletons/2215719b-1b0f-4890-a2f4-843bfef50f7d/in.py

---
+++ exercises/3dc610a8-88ea-4e91-b1ca-6edd1ca2c46d/skeletons/2215719b-1b0f-4890-a2f4-843bfef50f7d/in.py
@@ -0,0 +1,11 @@
+def sorting_hat(student_trait):
+    if student_trait == 'bravery':
+        return 'Gryffindor'
+    elif student_trait == 'hardworking':
+        return 'Hufflepuff'
+    elif student_trait == 'intelligence':
+        return 'Ravenclaw'
+    elif student_trait == 'cunning':
+        return 'Slytherin'
+    else:
+        return 'Unknown'
