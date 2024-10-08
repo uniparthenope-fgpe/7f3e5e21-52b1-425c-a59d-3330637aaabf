20144afeb533577d5303e92341b4281c191c81ed
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:30:52 +0200

    created file exercises/520af848-3b81-4b23-a76f-f0b01f5a48e9/skeletons/832b4e7c-87e4-4972-a4b8-437130061aef/in.py

---
+++ exercises/520af848-3b81-4b23-a76f-f0b01f5a48e9/skeletons/832b4e7c-87e4-4972-a4b8-437130061aef/in.py
@@ -0,0 +1,10 @@
+class Potion:
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+    # Add a method to mix the potion
+    def mix(self):
+        pass
+    # Add a method to check potency
+    def check_potency(self):
+        pass
