c5d85cd8c70959023af65953af8afc7045e12857
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:20:22 +0200

    created file exercises/4b1118e1-b1b5-476f-8096-8ee0fbdc182c/skeletons/73300c64-5046-4236-bcd9-9ab0028e938f/in.py

---
+++ exercises/4b1118e1-b1b5-476f-8096-8ee0fbdc182c/skeletons/73300c64-5046-4236-bcd9-9ab0028e938f/in.py
@@ -0,0 +1,9 @@
+potions = {}
+
+def add_potion(name, ingredients):
+    potions[name] = ingredients
+
+# Add a potion
+add_potion('Polyjuice Potion', [' lacewing flies', ' leeches'])
+
+# View potions
