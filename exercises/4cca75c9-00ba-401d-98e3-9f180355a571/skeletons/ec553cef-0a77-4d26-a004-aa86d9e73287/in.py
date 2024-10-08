6b935eca75de3a5d516f333cdc30d5c550c4df4d
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:42:47 +0200

    created file exercises/4cca75c9-00ba-401d-98e3-9f180355a571/skeletons/ec553cef-0a77-4d26-a004-aa86d9e73287/in.py

---
+++ exercises/4cca75c9-00ba-401d-98e3-9f180355a571/skeletons/ec553cef-0a77-4d26-a004-aa86d9e73287/in.py
@@ -0,0 +1,7 @@
+def calculate_speed(distance, time):
+    # Complete the function below
+    return _____
+
+distance = 1000  # in light-years
+ time = 5  # in years
+print('Starship speed: ' + str(calculate_speed(distance, time)) + ' light-years/year')
