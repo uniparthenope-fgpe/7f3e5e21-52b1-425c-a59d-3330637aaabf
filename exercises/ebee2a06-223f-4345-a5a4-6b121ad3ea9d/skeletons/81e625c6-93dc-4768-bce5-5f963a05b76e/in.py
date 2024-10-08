6e790385e06f4e0e6d7f4114e8bd7873701b3b40
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:23:20 +0200

    created file exercises/ebee2a06-223f-4345-a5a4-6b121ad3ea9d/skeletons/81e625c6-93dc-4768-bce5-5f963a05b76e/in.py

---
+++ exercises/ebee2a06-223f-4345-a5a4-6b121ad3ea9d/skeletons/81e625c6-93dc-4768-bce5-5f963a05b76e/in.py
@@ -0,0 +1,13 @@
+class Jedi:
+    def __init__(self, name):
+        self.name = name
+
+    def use_force(self):
+        # Complete this method
+        pass
+
+    def lightsaber_combat(self):
+        # Complete this method
+        pass
+
+luke = Jedi('Luke Skywalker')
