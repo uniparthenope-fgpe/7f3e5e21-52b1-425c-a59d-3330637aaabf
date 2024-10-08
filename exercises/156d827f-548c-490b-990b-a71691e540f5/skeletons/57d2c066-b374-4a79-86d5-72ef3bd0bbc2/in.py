5d9f520c8ce24777b50300f57fe6812b1eddea2e
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:56:36 +0200

    created file exercises/156d827f-548c-490b-990b-a71691e540f5/skeletons/57d2c066-b374-4a79-86d5-72ef3bd0bbc2/in.py

---
+++ exercises/156d827f-548c-490b-990b-a71691e540f5/skeletons/57d2c066-b374-4a79-86d5-72ef3bd0bbc2/in.py
@@ -0,0 +1,8 @@
+color = input('What is your favorite color? ')
+if color == 'blue':
+    lightsaber_color = 'Blue'
+elif color == 'green':
+    lightsaber_color = 'Green'
+else:
+    lightsaber_color = 'Red'
+print('Your lightsaber color is:', lightsaber_color)
