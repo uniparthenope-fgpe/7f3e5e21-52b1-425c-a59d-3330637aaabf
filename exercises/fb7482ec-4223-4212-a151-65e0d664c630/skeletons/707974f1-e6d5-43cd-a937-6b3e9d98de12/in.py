e1ad5700b1eff9eeb73f35a36a637003d8467555
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 22:31:51 +0200

    created file exercises/fb7482ec-4223-4212-a151-65e0d664c630/skeletons/707974f1-e6d5-43cd-a937-6b3e9d98de12/in.py

---
+++ exercises/fb7482ec-4223-4212-a151-65e0d664c630/skeletons/707974f1-e6d5-43cd-a937-6b3e9d98de12/in.py
@@ -0,0 +1,7 @@
+def mix_potion(ingredients):
+    if 'dragon blood' in ingredients:
+        return 'Elixir of Life'
+    elif 'mandrake' in ingredients:
+        return 'Mandrake Potion'
+    else:
+        return 'Unknown Potion'
