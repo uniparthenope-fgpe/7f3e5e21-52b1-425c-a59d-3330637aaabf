c35a636231fbc2a9a277f301be6c6dcc6a325bad
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 23:23:42 +0200

    created file exercises/ad49ad7d-ca0d-4dd6-a681-8ac6f680f918/solutions/4542773e-fe26-4260-a894-e5940955566d/sol.py

---
+++ exercises/ad49ad7d-ca0d-4dd6-a681-8ac6f680f918/solutions/4542773e-fe26-4260-a894-e5940955566d/sol.py
@@ -0,0 +1,9 @@
+def get_droid_name(droid_id):
+    if droid_id == 1:
+        return 'R2-D2'
+    elif droid_id == 2:
+        return 'C-3PO'
+    elif droid_id == 3:
+        return 'BB-8'
+    else:
+        return 'Unknown droid'
