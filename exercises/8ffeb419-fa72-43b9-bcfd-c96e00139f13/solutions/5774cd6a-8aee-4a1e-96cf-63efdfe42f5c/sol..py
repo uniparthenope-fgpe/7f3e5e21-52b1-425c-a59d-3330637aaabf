033b3093c9d7ef58bb3751135b76a7787bea5a30
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:18:47 +0200

    created file exercises/8ffeb419-fa72-43b9-bcfd-c96e00139f13/solutions/5774cd6a-8aee-4a1e-96cf-63efdfe42f5c/sol..py

---
+++ exercises/8ffeb419-fa72-43b9-bcfd-c96e00139f13/solutions/5774cd6a-8aee-4a1e-96cf-63efdfe42f5c/sol..py
@@ -0,0 +1,5 @@
+ingredient1 = 'Powdered Dragon Horn'
+ingredient2 = 'Bicorn Horn'
+quantity1 = 3
+quantity2 = 2
+print(f'To make the potion, you need {quantity1} of {ingredient1} and {quantity2} of {ingredient2}.')
