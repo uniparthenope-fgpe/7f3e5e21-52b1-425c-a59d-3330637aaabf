bc01072b9c0770d6f37765e998987ace033e1a36
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:06:55 +0200

    created file exercises/413732f3-f0ba-408a-a0a8-411741b008de/solutions/31dfaaea-4b94-4c50-ba18-163503756aad/sol..py

---
+++ exercises/413732f3-f0ba-408a-a0a8-411741b008de/solutions/31dfaaea-4b94-4c50-ba18-163503756aad/sol..py
@@ -0,0 +1,7 @@
+def mix_potion(ingredients):
+    if 'unicorn hair' in ingredients:
+        return 'Potion is successful!'
+    elif 'dragon blood' in ingredients:
+        return 'Potion is somewhat successful.'
+    else:
+        return 'Potion failed.'
