62786eab5b7b2e80c355e28db7c3d95c13367cff
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:05:31 +0200

    created file exercises/67b0687b-3f09-4007-b896-6ae74ca9568a/skeletons/48ce3b0a-322b-4c38-92e0-e5d67beef904/in..py

---
+++ exercises/67b0687b-3f09-4007-b896-6ae74ca9568a/skeletons/48ce3b0a-322b-4c38-92e0-e5d67beef904/in..py
@@ -0,0 +1,11 @@
+def time_travel(character, event):
+    # Logic to revisit past events
+    return new_event
+
+class Character:
+    def __init__(self, name):
+        self.name = name
+
+# Create character and event
+harry = Character('Harry')
+event = 'Battle of Hogwarts'
