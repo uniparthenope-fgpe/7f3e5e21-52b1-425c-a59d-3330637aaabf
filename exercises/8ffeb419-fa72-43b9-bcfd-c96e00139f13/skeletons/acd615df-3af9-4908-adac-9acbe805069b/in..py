138995efd63b92b19387983fb956be8e0f52a6b8
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:15:31 +0200

    created file exercises/8ffeb419-fa72-43b9-bcfd-c96e00139f13/skeletons/acd615df-3af9-4908-adac-9acbe805069b/in..py

---
+++ exercises/8ffeb419-fa72-43b9-bcfd-c96e00139f13/skeletons/acd615df-3af9-4908-adac-9acbe805069b/in..py
@@ -0,0 +1,5 @@
+ingredient1 = 'Powdered Dragon Horn'
+ingredient2 = 'Bicorn Horn'
+quantity1 = __
+quantity2 = __
+print(f'To make the potion, you need {quantity1} of {ingredient1} and {quantity2} of {ingredient2}.')
