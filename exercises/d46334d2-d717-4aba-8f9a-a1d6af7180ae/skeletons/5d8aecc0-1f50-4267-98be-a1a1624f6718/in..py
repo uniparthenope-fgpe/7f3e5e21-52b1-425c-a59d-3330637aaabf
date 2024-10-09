4e6601d1b9bfe5de87c4086e8d0d48fdca2b3070
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:16:55 +0200

    created file exercises/d46334d2-d717-4aba-8f9a-a1d6af7180ae/skeletons/5d8aecc0-1f50-4267-98be-a1a1624f6718/in..py

---
+++ exercises/d46334d2-d717-4aba-8f9a-a1d6af7180ae/skeletons/5d8aecc0-1f50-4267-98be-a1a1624f6718/in..py
@@ -0,0 +1,7 @@
+def mix_potion(ingredient1, ingredient2):
+    if ingredient1 == 'unicorn hair' and ingredient2 == 'dragon blood':
+        return 'Elixir of Life'
+    elif ingredient1 == 'toadstool' and ingredient2 == 'moonstone':
+        return 'Polyjuice Potion'
+    else:
+        return 'Unknown Potion'
