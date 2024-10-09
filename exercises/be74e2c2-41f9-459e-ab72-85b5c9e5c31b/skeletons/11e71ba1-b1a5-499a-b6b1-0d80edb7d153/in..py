46dbdae968f9b648dc40d726528f185d4ccbbd59
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:04:49 +0200

    created file exercises/be74e2c2-41f9-459e-ab72-85b5c9e5c31b/skeletons/11e71ba1-b1a5-499a-b6b1-0d80edb7d153/in..py

---
+++ exercises/be74e2c2-41f9-459e-ab72-85b5c9e5c31b/skeletons/11e71ba1-b1a5-499a-b6b1-0d80edb7d153/in..py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.spells_learned = []
+
+class Spell:
+    def __init__(self, name):
+        self.name = name
+        self.difficulty = 0
