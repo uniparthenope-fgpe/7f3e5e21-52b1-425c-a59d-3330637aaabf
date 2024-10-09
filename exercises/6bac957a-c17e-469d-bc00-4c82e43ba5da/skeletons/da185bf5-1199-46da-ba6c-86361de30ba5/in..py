6a63927a1c088d789b6290949769d03d3ec2ad0e
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 09:39:41 +0200

    created file exercises/6bac957a-c17e-469d-bc00-4c82e43ba5da/skeletons/da185bf5-1199-46da-ba6c-86361de30ba5/in..py

---
+++ exercises/6bac957a-c17e-469d-bc00-4c82e43ba5da/skeletons/da185bf5-1199-46da-ba6c-86361de30ba5/in..py
@@ -0,0 +1,8 @@
+class Spell:
+    def __init__(self, name, incantation, effect):
+        self.name = name
+        self.incantation = incantation
+        self.effect = effect
+
+    def ________:
+        # Your code here
