a22f2d22e0e354c97a44deb1b5236f9c19626b49
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-09 10:01:00 +0200

    created file exercises/9ce1ea42-86b9-4a37-9c58-eba054dbd4d6/solutions/bb96247d-ccaa-4994-8c97-1ec71a49fdea/sol..py

---
+++ exercises/9ce1ea42-86b9-4a37-9c58-eba054dbd4d6/solutions/bb96247d-ccaa-4994-8c97-1ec71a49fdea/sol..py
@@ -0,0 +1,4 @@
+potion_name = 'Polyjuice Potion'
+main_ingredient = 'lacewing flies'
+quantity = 2
+print('To brew the ' + potion_name + ', you need ' + str(quantity) + ' liters of ' + main_ingredient)
